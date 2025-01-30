71d12fd726392bb65d0a79e2c1d2f5169a183a1f
officiabreezy <isaacsegun2055@gmail.com>
2025-01-30 14:47:08 +0100

    Fix Vercel deployment

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ app.get('/', (req, res) => {
     });
 });
 
-app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`Server running on http://localhost:${port}`);
+// });
+
+module.exports = app;
